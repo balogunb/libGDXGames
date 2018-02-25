a5548fc290396c311e9766166ce28148d76d4b83
balogunb <you@example.com>
2018-02-25 18:22:11 -0500

    Creating a sample flappy birds game using libGDX

---
+++ FlappyBirdSample/desktop/src/com/mygdxflappy/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdxflappy.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdxflappy.game.FlappyB;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyB(), config);
+	}
+}
